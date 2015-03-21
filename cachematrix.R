1066920fe7db2ac1969682ee2a787a8d893f63b3
pstrope <poojastrope@gmail.com>
2015-03-20 23:08:48 -0400

    i am pushing hw2_test to see if git push works

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program calculates the matrix inverse. It creates a cache to save time so 
+## the inverse does not need to be calculated many times.  
+## 
 
-## Write a short comment describing this function
+## 
+## This function creates a list containing functions to set the matrix, 
+## get the matrix, set the inverse and get the inverse. When there is no cache m is 
+## NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <-function(solve) m <<-solve
+	getinverse <- function()m
+	list(set=set,get=get,
+	     setinverse=setinverse,
+	     getinverse=getinverse)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$setinverse(m)
+	m
 }
